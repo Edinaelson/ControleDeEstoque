8a164947caf0203283422429c79c6f867538dca2
Laboratório <laboratorio@copperline.com.br>
2023-10-18 17:01:01 -0300

    getter,setter,construtor

--- src/produto/Produto.java
+++ src/produto/Produto.java
@@ -6,6 +6,38 @@ public class Produto {
     public static int quantidade;
     public static double preco;
 
-    
+    public static String getNome() {
+        return nome;
+    }
 
+    public static void setNome(String nome) {
+        Produto.nome = nome;
+    }
+
+    public static int getCodigo() {
+        return codigo;
+    }
+
+    public static void setCodigo(int codigo) {
+        Produto.codigo = codigo;
+    }
+
+    public static int getQuantidade() {
+        return quantidade;
+    }
+
+    public static void setQuantidade(int quantidade) {
+        Produto.quantidade = quantidade;
+    }
+
+    public static double getPreco() {
+        return preco;
+    }
+
+    public static void setPreco(double preco) {
+        Produto.preco = preco;
+    }
+
+    public Produto() {
+    }
 }
