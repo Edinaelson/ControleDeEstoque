b377d1f133e7ec378f13e895232b50d7421a2bb7
Laboratório <laboratorio@copperline.com.br>
2023-10-18 16:39:14 -0300

    produto

--- src/produto/Produto.java
+++ src/produto/Produto.java
@@ -1,5 +1,11 @@
 package produto;
 
 public class Produto {
+    public static String nome;
+    public static int codigo;
+    public static int quantidade;
+    public static double preco;
+
+    
 
 }
