f6445760a1c4600c279a3f285d397a81d98cbf98
STELA MARIA RODRIGUES BEZERRA <202302401074@alunos.estacio.br>
2023-11-01 12:26:06 -0300

    implementando menu

---
+++ src/menu/Menu.java
@@ -0,0 +1,12 @@
+package menu;
+
+public class Menu {
+    public void menu(){
+        System.out.println("--------------------------------------------");
+        System.out.println("1- Cadastrar Produto");
+        System.out.println("2- Cadastrar Cliente");
+        System.out.println("3-Listar Clientes");
+        System.out.println("");
+        System.out.println("--------------------------------------------");
+    }
+}
